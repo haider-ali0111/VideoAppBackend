e4c75a913bc94c068cee3668c714d99fe18903cb
Rabnawaz <rabnawaz12@gmail.com>
2025-05-09 00:10:39 -0700

    first commit

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'production',
     PORT: process.env.PORT || 5000,
-    MONGO_URI: process.env.MONGO_URI || 'your_mongodb_connection_string',
-    JWT_SECRET: 'your-secure-jwt-secret-key-here',
+    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://haideraliak91:pW4lmU556BdBcYGn@videoapp.ezy2xek.mongodb.net/?retryWrites=true&w=majority&appName=VideoApp',
+    JWT_SECRET: '100',
     JWT_EXPIRE: '30d',
     JWT_COOKIE_EXPIRE: 30,
-    AZURE_STORAGE_CONNECTION_STRING: 'your_azure_storage_connection_string',
-    AZURE_STORAGE_CONTAINER_NAME: 'your_container_name'
+    AZURE_STORAGE_CONNECTION_STRING: process.env.AZURE_STORAGE_CONNECTION_STRING || 'DefaultEndpointsProtocol=https;AccountName=videodistributionstorage;AccountKey=SHhz7aR43i+eOKXde7+Sg0OpMw3UlJmLOl3W1e70GAN5qMi+vT1x6zyES+i7SOlAsGGgtiaCQQz9+AStluu5Zg==;EndpointSuffix=core.windows.net',
+    AZURE_STORAGE_CONTAINER_NAME: process.env.AZURE_STORAGE_CONTAINER_NAME || 'video-uploads'
 }; 
